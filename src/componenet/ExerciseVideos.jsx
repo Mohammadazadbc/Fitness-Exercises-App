ff1f9f7b8374c00810a4535f85341adb4f30864a
mohammad azad <mohammadazad.bc@gmail.com>
2022-09-02 21:27:43 +0200

    add loader to vidoe

--- src/componenet/ExerciseVideos.jsx
+++ src/componenet/ExerciseVideos.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Box, Stack, Typography } from "@mui/material"
+import Loader from "./Loader"
 function ExerciseVideos({videoExericse, name}) {
 
-  if(!videoExericse.length) return 'Loading...'
+  if(!videoExericse.length) return <Loader />
   return (
     <Box sx={{marginTop:{ lg:'200px', xs : '20px'}}}>
         <Typography variant='h3' marginBottom='33px'>Watch <span style={{color:'#ff2625', textTransform:'capitalize'}}> {name} </span> exercise videos </Typography>
